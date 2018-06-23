5ade1e08c379d9cf55c8f3e65eb16a8c1450eec6
Terry Caliendo <githubcom@dedicatedmanagers.com>
2018-06-22 21:15:52 -0700

    tried to create a global confirmation dialog box.  had issues with passing context to a function that would be called from within the object within the store; context wouldn't pass through - errored as not defined

--- src/App.vue
+++ src/App.vue
@@ -1,5 +1,23 @@
 <template>
   <v-app>
+    <template>
+      <v-layout row justify-center v-if="confirmDialogIsVisible">
+        <v-dialog v-model="confirmDialogIsVisible" persistent max-width="290">
+          <v-btn slot="activator" color="primary" dark>Open Dialog</v-btn>
+          <v-card>
+            <v-card-title class="headline">Use Google's location service?</v-card-title>
+            <v-card-text>Let Google help apps determine location. This means sending anonymous location data to Google, even when no apps are running.</v-card-text>
+            <v-card-actions>
+              <v-spacer></v-spacer>
+              <v-btn color="green darken-1" flat @click="confirmDialogDecision(false)">Disagree</v-btn>
+              <v-btn color="green darken-1" flat @click="confirmDialogDecision(false)">Agree</v-btn>
+            </v-card-actions>
+          </v-card>
+        </v-dialog>
+      </v-layout>
+    </template>
+
+
     <v-toolbar app clipped>
       <v-btn icon to="/dashboard">
         <v-icon>dashboard</v-icon>
@@ -30,9 +48,13 @@ export default {
   name: 'App',
   data() {
     return {
+      
     };
   },
   computed:{
+    confirmDialogIsVisible(){
+      return this.$store.state.confirmDialog.isVisible;
+    },
     userIsAuthenticated: function(){
       return this.$store.state.userIsAuthenticated;
     },
@@ -43,7 +65,15 @@ export default {
     },
     logout(){
       this.$store.dispatch('logout');
-    }
+    },
+    confirmDialogDecision(result) {
+      if(result){
+        return this.$store.state.confirmDialog.resultTrueFunction();
+      }
+      else{
+        return this.$store.state.confirmDialog.resultFalseFunction();          
+      }
+    },
   }
 };
 </script>
--- src/store/store.js
+++ src/store/store.js
@@ -7,6 +7,11 @@ Vue.use(Vuex);
 
 export const store = new Vuex.Store({
     state:{
+        confirmDialog:{
+            isVisible:false,
+            resultTrueFunction:function(){alert('true');return true;},
+            resultFalseFunction:function(){alert('false');return false;},
+        },
         user:null,
         userIsAuthenticated:false,
         QUERY_PrimaryRelativeCaregiverById:false,
@@ -15,6 +20,13 @@ export const store = new Vuex.Store({
 
     },
     mutations:{
+        setConfirmDialogByObject(state, confirmDialogProperties){
+            for (var key in confirmDialogProperties) {
+                if (confirmDialogProperties.hasOwnProperty(key)) {
+                    state.confirmDialog[key] = confirmDialogProperties[key];
+                }
+            }
+        },
         setUserIsAuthenticated(state, replace){
             state.userIsAuthenticated = replace;
         },
@@ -110,6 +122,16 @@ export const store = new Vuex.Store({
         },
         // Delete Client
         fdelete_PrimaryRelativeCaregiverById(context){
+            context.commit('setConfirmDialogByObject', {
+                isVisible:true,
+                //resultTrueFunction:context=>{console.log('resultTrueFunction');context.commit('setConfirmDialogByObject',{isVisible:false})},
+                //resultFalseFunction:context=>{console.log('resultFalseFunction');context.commit('setConfirmDialogByObject',{isVisible:false})},
+            });
+            console.log(context);
+            context.state.confirmDialog.resultTrueFunction=function(context){context.state.confirmDialog.isVisible=false;};
+            context.state.confirmDialog.resultFalseFunction=function(context){context.state.confirmDialog.isVisible=false;};
+
+            return false;
             firebase.firestore().collection('PrimaryRelativeCaregiver').doc(context.state.currentPrimaryRelativeCaregiver.id).delete()
                 .then(function(docRef) {
                     router.replace('/dashboard');               
